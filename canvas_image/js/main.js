5fa2c6feb5d603fa2e8a5871094375e29f074695
James <james.l.kao@gmail.com>
2015-02-07 15:48:56 -0800

    added comments

--- canvas_image/js/main.js
+++ canvas_image/js/main.js
@@ -28,23 +28,29 @@ function main(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     resize_canvas();
     
-    
+    // Draw a red rectangle to the screen 
     ctx.fillStyle = "red";
     ctx.fillRect(10, 10, 50, 50);
     
-    imgData=ctx.getImageData(10,10,50,50);  // image data saved in the form of [p1_red, p1_green, p1_blue, p1_alpha, p2_red....]
-    ctx.putImageData(imgData,10,70);
+    // Grab image data from the canvas starting at pixel 10, 10
+    // This is the object that we draw onto the screen
+    imgData=ctx.getImageData(10,10,50,50); 
     
+    // Clear the screen again so we can draw the inverted image
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
+    // Grab the actual pixel values from the graphical object
+    // image data saved in the form of [p1_red, p1_green, p1_blue, p1_alpha, p2_red....]
     data = imgData.data;
     
+    // Iterate through all the pixels and invert them
     for (var i = 0; i < data.length; i += 4) {
         data[i]     = 225 - data[i];     // red
         data[i + 1] = 225 - data[i + 1]; // green
         data[i + 2] = 225 - data[i + 2]; // blue
     }
-        
+    
+    // Draw the image back onto the canvas
     ctx.putImageData(imgData, 0, 0);
     
     //start();
