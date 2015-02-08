8fbf840b8b4abef9bba1776a965fd373f64d9f65
James <james.l.kao@gmail.com>
2015-02-07 17:03:35 -0800

    added scrolling background

--- main/js/main.js
+++ main/js/main.js
@@ -1,15 +1,24 @@
-// branch_test!!
-
 var canvas = document.getElementsByTagName("canvas")[0];
 var ctx = canvas.getContext("2d");
 
-var FRAME_RATE = 30;
+var i_x = document.getElementById("i_x");
+var i_y = document.getElementById("i_y");
+
+var INTERVAL = 30;
 var user;
 var _args = {};
 
+var HEIGHT = canvas.height;
+var WIDTH = canvas.width;
+
+var background_x;
+var background_y;
+
+var background_image;
+
 function user(x, y){
-    this.x = 100;
-    this.y = 100;
+    this.x = 500;
+    this.y = 500;
     this.sprite;
     this.draw = function(){
         ctx.putImageData(this.sprite, this.x, this.y); 
@@ -17,6 +26,19 @@ function user(x, y){
     }
 }
 
+
+// SETTINGS
+
+var show_information = true;
+
+var x_min = 400;      // the minimum x-value before the background scrolls
+var x_max = 800;      // the maximum x-value before the background scrolls
+
+
+
+
+
+// MAIN
 
 var hab = hab || (function(){
     
@@ -31,24 +53,26 @@ var hab = hab || (function(){
     };
 }());
 
-
-
 function main(){
     // Clear the canvas and resize it
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     resize_canvas();
+    
+    background_img = new Image();
+    background_img.src = "img/bg_1.jpg"
+    background_x = 100;
+    background_y = 100;
+    
     
     user = new user(0, 0);
     ctx.fillStyle = "red";
     ctx.fillRect(10, 10, 50, 50);
     user.sprite = ctx.getImageData(10,10,50,50);
     
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    
     var data = user.sprite.data;
     
-    console.log(data[0]);
-    console.log(data[1]);
-    console.log(data[2]);
-    console.log(data[3]);
     
     
     init_keyboardevents();
@@ -73,20 +97,16 @@ function key_events(event){
             
             
         case 39:        // RIGHT arrow key
-            user.x += 1;
-            console.log(user.x);
+            move(3);
             break;
         case 37:        // RIGHT arrow key
-            user.x -= 1;
-            console.log(user.x);
+            move(-3);
             break;
         case 38:        // UP arrow key
-            user.y -= 1;
-            console.log(user.x);
+            user.y -= 3;
             break;
         case 40:        // LEFT arrow key
-            user.y += 1;
-            console.log(user.x);
+            user.y += 3;
             break;
     }
 }
@@ -94,7 +114,7 @@ function key_events(event){
 function start(){
     loop = setInterval(function(){
             draw();
-        }, FRAME_RATE
+        }, INTERVAL
     )
 }
 
@@ -107,11 +127,14 @@ function stop(){
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     resize_canvas();
+    
     draw_background();
     user.draw();
+    update_information();
 }
 
 function draw_background(){
+    ctx.drawImage(background_img, background_x, background_y, 2000, 300);
 }
 
 //////////// UTILITY FUNCTIONS
@@ -123,4 +146,32 @@ function get_color_string(r, g, b, a){
 function resize_canvas(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+}
+
+function update_information(){
+    i_x.innerHTML = "x: " + user.x;
+    i_y.innerHTML = "y: " + user.y;
+}
+
+//////////// USER MOVEMENT
+
+function move(value){
+    if (value > 0){
+        // Moving in the RIGHT direction
+        if (user.x >= x_max){
+            background_x -= value;
+        } else {
+            user.x += value;
+        }
+        
+        
+    } else {
+        // Moving in the lEFT direction
+        if (user.x <= x_min){
+            background_x += value;
+        } else {
+            user.x -= value;
+        }
+        
+    }
 }
