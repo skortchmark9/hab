422117a6056077567dfcb89b3340ae30ae0ba5cb
sandmatt <msandmatt@gmail.com>
2015-02-07 20:20:27 -0500

    mousefix 2

--- canvas_image/js/main.js
+++ canvas_image/js/main.js
@@ -7,37 +7,6 @@ var gridSize = canvasSize/numPixels;
 console.log(gridSize);
 
 
-
-function main(){
-    // Clear the canvas and resize it
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    resize_canvas();
-    
-    // Draw a red rectangle to the screen 
-    ctx.fillStyle = "red";
-    ctx.fillRect(10, 10, 50, 50);
-    
-    // Grab image data from the canvas starting at pixel 10, 10
-    imgData=ctx.getImageData(10,10,50,50); 
-    
-    // Clear the screen again so we can draw the inverted image
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    // Grab the actual pixel values from the graphical object
-    // image data saved in the form of [p1_red, p1_green, p1_blue, p1_alpha, p2_red....]
-    data = imgData.data;
-    
-    // Iterate through all the pixels and invert them
-    for (var i = 0; i < data.length; i += 4) {
-        data[i]     = 225 - data[i];     // red
-        data[i + 1] = 225 - data[i + 1]; // green
-        data[i + 2] = 225 - data[i + 2]; // blue
-    }
-    
-    // Draw the image back onto the canvas
-    ctx.putImageData(imgData, 0, 0);
-}
-
 ctx.fillStyle = "#FF0010";
 ctx.fillRect(0,0,c.width,c.height);
 
@@ -78,5 +47,7 @@ c.addEventListener("mousemove", function(evt) {
   var message = 'Mouse position: ' + Math.ceil(mousePos.x/gridSize) + ',' + Math.ceil(mousePos.y/gridSize);
   console.log(message);
 }, false);
+
+
 
 //ctx.clearRect(0,0,canvasSize,canvasSize);
