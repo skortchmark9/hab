d94b3b7d4fc8f0a87170ed8d435bbc08c6c2c3b7
James <james.l.kao@gmail.com>
2015-02-07 22:20:49 -0800

    reuploaded

--- main/js/main.js
+++ main/js/main.js
@@ -31,7 +31,11 @@ function user(x, y){
     
     this.walking = false;
     this.jumping = false;
-    this.jump_frame = 0;
+    this.landed = true;
+    
+    this.walking_loop;
+    this.walking_frame = 0;
+    this.walking_sprite_list = new Array();
     
     this.draw = function(){
         ctx.putImageData(this.sprite, this.x, this.y);
@@ -55,11 +59,12 @@ function user(x, y){
 
 var show_information = true;
 
-var x_min = 400;      // the minimum x-value before the background scrolls
-var x_max = 800;      // the maximum x-value before the background scrolls
-var ground_level = 800;     // ground level measured from the top of the page
+var x_min = 400;                // the minimum x-value before the background scrolls
+var x_max = 800;                // the maximum x-value before the background scrolls
+var ground_level = 800;         // ground level measured from the top of the page
 var friction = 0.8;
 var gravity = 0.3;
+var walking_interval = 500;     // the delay before the frame for a walking animation changes
 
 
                                                                                                                 //////////// MAIN
@@ -104,13 +109,20 @@ function main(){
     user.sprite_list.push(color_to_imgdata("blue", 50, 50));
     user.sprite_list.push(color_to_imgdata("red", 50, 50));
     
+    user.walking_sprite_list.push(color_to_imgdata("green", 50, 50));
+    user.walking_sprite_list.push(color_to_imgdata("yellow", 50, 50));
+    
     user.sprite = user.sprite_list[0];
 //    user.sprite = url_to_imgdata("img/sprite_1.png", 50, 50); // contamination by cross-origin data
     
     init_keyboardevents();
+    
+    window.addEventListener("load", function(){
+        update();
+    });
 }
 
-//////////// DRAWING
+                                                                                                                //////////// DRAWING and UPDATE
 
 function update(){
     update_movement();
@@ -131,33 +143,7 @@ function draw_background(){
     ctx.drawImage(background_img, background_x, background_y, 2000, 300);
 }
 
-//////////// UTILITY FUNCTIONS
-
-function init_keyboardevents(){
-//    window.addEventListener('keydown', key_events, false);
-    document.body.addEventListener("keydown", function(e) {
-        keys[e.keyCode] = true;
-    });
-    document.body.addEventListener("keyup", function(e) {
-        keys[e.keyCode] = false;
-    });
-}
-
-function resize_canvas(){
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
-
-function update_information(){
-    i_x.innerHTML = "x: " + user.x;
-    i_y.innerHTML = "y: " + user.y;
-}
-
-function get_color_string(r, g, b, a){
-    return "rgba(" + r + "," + g + "," + b + "," + a + ")";
-}
-
-//////////// USER MOVEMENT
+                                                                                                                //////////// USER MOVEMENT
 
 function move(value){
     if (value > 0){
@@ -182,6 +168,7 @@ function update_movement(){
         // up arrow or space
         if(!user.jumping){
             user.jumping = true;
+            user.started_jumping = true;
             user.velY = -user.speed*2;
             user.sprite = user.sprite_list[1];
             console.log("Jumping");
@@ -190,18 +177,27 @@ function update_movement(){
     }
     
     if (keys[39]) {
+        console.log("right");
         // right arrow
         if (!user.walking){
             user.walking = true;
             console.log("Moving Right");
-            i_movement.innerHTML = "movement: Moving Right"; 
+            i_movement.innerHTML = "movement: Moving Right";
         }
+        
+//        if (!user.jumping){
+//            user.walking_loop = setInterval(function(){
+//                    next_walking_frame();
+//                }, walking_interval
+//            )
+//        }
         
         if (user.velX < user.speed) {                         
             user.velX++;                  
         }
     }
-    if (keys[37]) {                 
+    if (keys[37]) {   
+        console.log("left");              
         // left arrow
         if (!user.walking){
             user.walking = true;
@@ -225,20 +221,47 @@ function update_movement(){
     }
     
     user.velX *= friction;
-    user.velY += gravity;
     
+    if (user.jumping){
+        user.velY += gravity;
+    }
+    
+    console.log("before moving: " + user.y);
+    console.log("current velocity: " + user.velY);
     move(user.velX);
-    user.y += user.velY;
     
-    if (user.y >= ground_level) {
+//    if (user.y != ground_level){
+//        console.log("Adding velocity");
+//    }
+    
+    user.y += user.velY;
+    console.log(user.y);
+    
+    // ground_level + gravity 
+    if (user.y > ground_level) {
         user.y = ground_level;
         user.jumping = false;
+        if (!user.landed){
+            user.landed = true;
+        }
+        
         // Return to resting sprite
-        user.sprite = user.sprite_list[0];
+//        console.log("Landed");
+//        user.sprite = user.sprite_list[0];
     }
+    
+//    if (user.landed){
+//        
+//    }
+    
 }
 
-//////////// IMGDATA
+function next_walking_frame(){
+    user.walking_frame = user.walking_frame + 1 % user.walking_sprite_list.length;
+    user.sprite = user.walking_sprite_list[user.walking_frame];
+}
+
+                                                                                                                //////////// IMGDATA
 
 // call these at the start of main
 // Setting height or width to -1 creates a sprite with the original height/width
@@ -276,6 +299,30 @@ function color_to_imgdata(color, width, height){
     return imgData;
 }
 
-window.addEventListener("load", function(){
-    update();
-});
+
+
+                                                                                                                //////////// UTILITY FUNCTIONS
+
+function init_keyboardevents(){
+//    window.addEventListener('keydown', key_events, false);
+    document.body.addEventListener("keydown", function(e) {
+        keys[e.keyCode] = true;
+    });
+    document.body.addEventListener("keyup", function(e) {
+        keys[e.keyCode] = false;
+    });
+}
+
+function resize_canvas(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+function update_information(){
+    i_x.innerHTML = "x: " + user.x;
+    i_y.innerHTML = "y: " + user.y;
+}
+
+function get_color_string(r, g, b, a){
+    return "rgba(" + r + "," + g + "," + b + "," + a + ")";
+}
