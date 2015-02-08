0ec90a3cc7e5c34118e28c7b928900e2b841f6fb
skortchmark9 <samuel_kortchmar@brown.edu>
2015-02-07 21:20:06 -0500

    jquery + color picker

--- canvas_image/js/main.js
+++ canvas_image/js/main.js
@@ -4,35 +4,35 @@ var ctx = c.getContext("2d");
 var canvasSize = c.width;
 var numPixels = 8;
 var gridSize = canvasSize/numPixels;
-
+var currentColor;
 
 
 function main(){
     // Clear the canvas and resize it
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     resize_canvas();
-    
-    // Draw a red rectangle to the screen 
+
+    // Draw a red rectangle to the screen
     ctx.fillStyle = "red";
     ctx.fillRect(10, 10, 50, 50);
-    
+
     // Grab image data from the canvas starting at pixel 10, 10
-    imgData=ctx.getImageData(10,10,50,50); 
-    
+    imgData=ctx.getImageData(10,10,50,50);
+
     // Clear the screen again so we can draw the inverted image
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+
     // Grab the actual pixel values from the graphical object
     // image data saved in the form of [p1_red, p1_green, p1_blue, p1_alpha, p2_red....]
     data = imgData.data;
-    
+
     // Iterate through all the pixels and invert them
     for (var i = 0; i < data.length; i += 4) {
         data[i]     = 225 - data[i];     // red
         data[i + 1] = 225 - data[i + 1]; // green
         data[i + 2] = 225 - data[i + 2]; // blue
     }
-    
+
     // Draw the image back onto the canvas
     ctx.putImageData(imgData, 0, 0);
 }
@@ -52,17 +52,18 @@ for (var y = 0.0; y < c.height; y += gridSize) {
 }
 
 
-function drawpixel(pixFillx, pixFilly, color){
+function drawPixel(pixFillx, pixFilly, color){
+  color = color || currentColor;
+
   ctx.fillStyle = color;
   ctx.fillRect((pixFillx-1)*gridSize,(pixFilly-1)*gridSize,gridSize,gridSize);
 }
 
-drawpixel(3,4,"#CC99FF");
-drawpixel(3,7,"#0099FF");
+drawPixel(3,4,"#CC99FF");
+drawPixel(3,7,"#0099FF");
 
 ctx.strokeStyle = "#ddd";
 ctx.stroke();
-
 
 
 //ctx.clearRect(0,0,canvasSize,canvasSize);
